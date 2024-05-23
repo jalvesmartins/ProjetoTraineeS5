0aed718d7a5dad0ca22a2ba7b6c6cd147d175a5a
Bernardo Viggiano <beviggiano@gmail.com>
2024-05-23 17:45:14 -0300

    feat(UserMusicServices): criação entidade UserMusic e escopo métodos

---
+++ src/domains/userMusic/services/UserMusicServices.ts
@@ -0,0 +1,26 @@
+import { UserMusic } from "@prisma/client";
+import { Prisma } from "@prisma/client";
+
+class UserMusicService {
+    //Adiciona música a um usuário
+    async addMusicToUser(){
+
+    }
+
+    //Listar as músicas de um usuário
+    async getMusicsByUser(){
+
+    }
+
+    //Remove uma música de um usuário 
+    async removeMusicFromUser(){
+
+    }
+
+    //Verifica se um usuário já escutou determinada música
+    async hasUserHeardMusic(){
+
+    }
+}
+
+export default UserMusicService;
