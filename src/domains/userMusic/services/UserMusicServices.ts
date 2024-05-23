3f2b50cf2ae48c21e35b96bc58162e241574afc3
Bernardo Viggiano <beviggiano@gmail.com>
2024-05-23 17:49:52 -0300

    feat(UserMusicServices): parâmetro funções adicionados

--- src/domains/userMusic/services/UserMusicServices.ts
+++ src/domains/userMusic/services/UserMusicServices.ts
@@ -3,23 +3,23 @@ import { Prisma } from "@prisma/client";
 
 class UserMusicService {
     //Adiciona música a um usuário
-    async addMusicToUser(){
+    async addMusicToUser(userId: number, musicId: number){
 
     }
 
     //Listar as músicas de um usuário
-    async getMusicsByUser(){
+    async getMusicsByUser(userId: number){
 
     }
 
     //Remove uma música de um usuário 
-    async removeMusicFromUser(){
+    async removeMusicFromUser(userId: number, musicId: number){
 
     }
 
     //Verifica se um usuário já escutou determinada música
-    async hasUserHeardMusic(){
-
+    async hasUserHeardMusic(userId: number, musicId: number): Promise<boolean> {
+        return ;
     }
 }
 
