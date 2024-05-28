cdef1eb23b7ffdeea5aa271d37ab66237258071b
Bernardo Viggiano <beviggiano@gmail.com>
2024-05-28 15:48:51 -0300

    feat(index): criação rota para retornar todos os artistas

--- config/expressConfig.ts
+++ config/expressConfig.ts
@@ -1,23 +1,20 @@
-import cors, { CorsOptions } from "cors";
-import dotenv from "dotenv";
-import express, { Express } from "express";
-import ArtistRouter from "../src/domains/artist/controllers";
+import cors, { CorsOptions } from 'cors';
+import dotenv from 'dotenv';
+import express, { Express } from 'express';
+import ArtistRouter from '../src/domains/artist/controllers';
 
 dotenv.config();
 
 export const app: Express = express();
 
-const options: CorsOptions = { 
+const options: CorsOptions = {
     credentials: true,
     origin: process.env.APP_URL
 };
 
 app.use(cors(options));
 app.use(express.json());
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(express.urlencoded({ extended: true }));
 
-//implementar rotas
-
-app.use("api/artist", ArtistRouter);
+// Implementar rotas
+app.use('/api/artist', ArtistRouter);
--- src/domains/artist/controllers/index.ts
+++ src/domains/artist/controllers/index.ts
@@ -14,4 +14,14 @@ router.post('/', async (req: Request, res: Response, next: NextFunction) => {
     }
 });
 
+// Rota para retornar todos os artistas
+router.get('/', async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const artists = await artistService.readAll();
+        res.status(200).json(artists);
+    } catch (error) {
+        next(error);
+    }
+});
+
 export default router; 
