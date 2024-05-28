660e723d0cf03756df00117044cfae34c86e9219
Bernardo Viggiano <beviggiano@gmail.com>
2024-05-28 09:53:21 -0300

    fix(ArtistService): conflits

--- src/domains/artist/services/ArtistServices.ts
+++ src/domains/artist/services/ArtistServices.ts
@@ -1,105 +1,61 @@
-import { User } from '.prisma/client';
+import { Artist } from '@prisma/client';
 import prisma from '../../../../config/prismaClient'
-
-class ServiceUser {
-    //Cria um usuário
-    async create(body: User) {
-        const createUser = await prisma.user.create({
+    
+class ArtistService {
+    //Cria um novo artista
+    async create(body: Artist) {
+        const artist = await prisma.artist.create({
             data: {
+                id: body.id,
                 name: body.name,
-                email: body.email,
                 photo: body.photo,
-                password: body.password,
-                role: body.role
+                stream: body.stream
             }
-        });
-
-        return createUser;
+        })
+        return artist;
     }
 
-    //Retorna todos os usuários
+    //Retorna todos os artistas
     async readAll() {
-        const readUser = await prisma.user.findMany();
-        return readUser;
+        const artists = await prisma.artist.findMany();
+        return artists;
     }
 
-    //Retorna um usuário pelo ID
+    //Retorna um artista pelo ID
     async readById(id: number) {
-        const readUserId = await prisma.user.findUnique({
+        const artist = await prisma.artist.findUnique({
             where: { id: id }
         });
-        return readUserId;
+        return artist;
     }
 
-    //Atualiza um usuário pelo ID
-    async update(id: number, body: Partial<User>) {
+    //Atualiza um artista pelo ID
+    async update(id: number, body: Partial<Artist>) {
         const updateData = {
-                ...(body.name && { name: body.name }),
-                ...(body.email && { email: body.email }),
-                ...(body.photo && { photo: body.photo }),
-                ...(body.password && { password: body.password }),
-                ...(body.role && { role: body.role }),
-            };
-
-        const updatedUser = await prisma.user.update({
+            ...(body.name && { name: body.name }),
+            ...(body.photo && { photo: body.photo }),
+            ...(body.stream && { stream: body.stream })
+        };
+    
+        const updatedMusic = await prisma.artist.update({
             where: { id: id },
             data: updateData
         });
-
-        const user = await prisma.user.findUnique({
-            where: { id: id },
+    
+        const artist = await prisma.artist.findUnique({
+            where: { id: id }
         });
+    
+        return artist;
+    }    
 
-        return user;
-    }
-
-    //Deleta um usuário pelo ID
+    //Deleta um artista pelo ID
     async delete(id: number) {
-        const deletedUser = await prisma.user.findUnique({
+        const artist = await prisma.artist.delete({
             where: { id: id }
-        })
-        const userDelete = await prisma.user.delete({
-            where: { id: id }
-        })
-        return deletedUser;
-    }
-
-    //Adiciona música a um usuário
-    async addMusicToUser(userId: number, musicId: number) {
-        const updatedUser = await prisma.user.update({
-            where: { id: userId },
-            data: {
-                musics: {
-                    connect: { id: musicId },
-                },
-            },
-            include: { musics: true },
         });
-        return updatedUser;
+        return artist;
     }
-        
-    //Remove uma música de um usuário 
-    async removeMusicFromUser(userId: number, musicId: number) {
-        const updatedUser = await prisma.user.update({
-            where: { id: userId },
-            data: {
-                musics: {
-                    disconnect: { id: musicId },
-                },
-            },
-            include: { musics: true },
-        });
-        return updatedUser;
-    }
+} 
 
-    //Lista as músicas já escutadas por determinado usuário
-    async musicsListenByUser(userId: number){
-        const musicsByUser = await prisma.user.findUnique({
-            where:{ id: userId },
-            select:{ musics: true }
-        })
-        return musicsByUser;
-    }
-}
-
-export default new ServiceUser();
+export default ArtistService;
