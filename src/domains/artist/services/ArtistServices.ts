5031d0a0e2b5caac9b5fe5f373ed673a455869ca
Bernardo Viggiano <beviggiano@gmail.com>
2024-06-04 10:24:19 -0300

    feat(ArtistServices): lançamento exceção em readById

--- src/domains/artist/services/ArtistServices.ts
+++ src/domains/artist/services/ArtistServices.ts
@@ -1,5 +1,7 @@
 import { Artist } from '@prisma/client';
 import prisma from '../../../../config/prismaClient'
+import { QueryError } from '../../../../errors/QueryError';
+import { InvalidParamError } from '../../../../errors/InvalidParamError';
     
 class ArtistService {
     //Cria um novo artista
@@ -17,7 +19,6 @@ class ArtistService {
 
     //Retorna todos os artistas
     async readAll() {
-
         const artists = await prisma.artist.findMany({
             orderBy: {
                 name: 'asc'
@@ -28,9 +29,18 @@ class ArtistService {
 
     //Retorna um artista pelo ID
     async readById(id: number) {
+        if(!id){
+            throw new InvalidParamError("É necessário informar um ID")
+        }
+
         const artist = await prisma.artist.findUnique({
             where: { id: id }
         });
+
+        if(!artist){
+            throw new QueryError("Artista não encontrado")
+        }
+        
         return artist;
     }
 
